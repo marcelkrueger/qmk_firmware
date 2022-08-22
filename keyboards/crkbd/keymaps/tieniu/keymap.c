514751e7e35a01d61e170f5d0721322287ffefa0
Marcel Krueger <krueger@vr.rwth-aachen.de>
2022-08-22 19:30:31 +0200

    corne keymap update

--- keyboards/crkbd/keymaps/tieniu/keymap.c
+++ keyboards/crkbd/keymaps/tieniu/keymap.c
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "keycode.h"
 #include "keycode_legacy.h"
 #include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
@@ -49,17 +50,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_RALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  MT(KC_RCTL,KC_ESC), //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI, KC_RAISE, KC_LCTL,    KC_RSFT, KC_LOWER, KC_LALT
+                                        MT(MOD_LGUI, KC_ESC), KC_RAISE, KC_LCTL,    KC_RSFT, KC_LOWER, KC_LGUI
                                       //`--------------------------'  `------------LOWER----------'
 
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+      QK_MAKE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_END, KC_PGUP , KC_PGDN, KC_HOME, KC_DEL,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -73,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        _______, KC_EXLM,   KC_AT, KC_HASH, KC_DLR, KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, KC_LCBR, TD(TD_LP_RP), TD(TD_LC_RC),KC_MINS,  KC_EQL, KC_BSLS, KC_GRV, KC_COLN ,  KC_DQT,
+      _______, XXXXXXX, XXXXXXX, KC_LCBR, TD(TD_LP_RP), TD(TD_LC_RC),           KC_MINS,   KC_EQL, KC_BSLS, KC_GRV, KC_COLN ,  KC_DQT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, KC_RCBR, TD(TD_LB_RB), TD(TD_LA_RA),            KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, XXXXXXX, XXXXXXX,
+      _______, XXXXXXX, XXXXXXX, KC_RCBR, TD(TD_LB_RB), TD(TD_LA_RA),           KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______,  _______,     _______, _______, _______
                                       //`--------------------------'  `--------------------------'
